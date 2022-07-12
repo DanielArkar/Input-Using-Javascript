5c96282ba801a84a31ccb90d7b11ca8e19199e23
Daniel <danielarkar36@gmail.com>
2022-07-12 10:13:41 +0630

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const productNameInputElement = document.getElementById("product-name");
+const remainingCharsElement = document.getElementById("remaining-chars");
+
+// console.dir(productNameInputElement);
+
+const maxAllowedChars = productNameInputElement.maxLength;
+
+function updateRemainingCharacters(event) {
+  const enteredText = event.target.value;
+  const enteredTextLength = enteredText.length;
+
+  const remainingCharacters = maxAllowedChars - enteredTextLength;
+
+  remainingCharsElement.textContent = remainingCharacters;
+
+  if (remainingCharacters === 0) {
+    productNameInputElement.classList.add("error");
+    remainingCharsElement.classList.add("error");
+  } else if (remainingCharacters <= 10) {
+    productNameInputElement.classList.add("warning");
+    remainingCharsElement.classList.add("warning");
+  } else {
+    productNameInputElement.classList.remove("error", "warning");
+    remainingCharsElement.classList.remove("error", "warning");
+  }
+}
+
+productNameInputElement.addEventListener("input", updateRemainingCharacters);
